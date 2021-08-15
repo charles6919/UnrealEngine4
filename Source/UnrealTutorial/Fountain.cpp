6a840afa07dc996afbea42ca619d376d4ca68914
charles6919 <charles6919@gmail.com>
2021-08-15 12:48:53 +0900

    Chapter 3 움직이는 Actor 제작 - 1  UE4 튜토리얼

--- Source/UnrealTutorial/Fountain.cpp
+++ Source/UnrealTutorial/Fountain.cpp
@@ -7,12 +7,13 @@
 AFountain::AFountain()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 
 	Body = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BODY"));
 	Water = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("WATER"));
 	Light = CreateDefaultSubobject<UPointLightComponent>(TEXT("LIGHT"));
 	Splash = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("SPLASH"));
+	Movement = CreateDefaultSubobject<URotatingMovementComponent>(TEXT("MOVEMENT"));
 
 	RootComponent = Body;
 	Water->SetupAttachment(Body);
@@ -29,7 +30,7 @@ AFountain::AFountain()
 		Body->SetStaticMesh(SM_BODY.Object);
 
 	static ConstructorHelpers::FObjectFinder<UStaticMesh>
-		SM_WATER(TEXT("/Game/InfinityBladeGrassLands/Effects/FX_Materials/Liquid/M_Water_fountain_01.M_Water_fountain_01"));
+		SM_WATER(TEXT("/Game/InfinityBladeGrassLands/Effects/FX_Meshes/Env/SM_Plains_Fountain_02.SM_Plains_Fountain_02"));
 	if (SM_WATER.Succeeded())
 		Water->SetStaticMesh(SM_WATER.Object);
 
@@ -37,6 +38,9 @@ AFountain::AFountain()
 		PS_SPLASH(TEXT("/Game/InfinityBladeGrassLands/Effects/FX_Ambient/Water/P_Water_Fountain_Splash_Base_01.P_Water_Fountain_Splash_Base_01"));
 	if (PS_SPLASH.Succeeded())
 		Splash->SetTemplate(PS_SPLASH.Object);
+
+	RotateSpeed = 30.0f;
+	Movement->RotationRate = FRotator(0.0f, RotateSpeed, 0.0f);
 }
 
 // Called when the game starts or when spawned
@@ -48,12 +52,24 @@ void AFountain::BeginPlay()
 	ULOG(Warning, TEXT("Actor Name : %s, ID : %d, Location X : %.3f"), *GetName(), ID, GetActorLocation().X);
 }
 
+void AFountain::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	Super::EndPlay(EndPlayReason);
+	ULOG_S(Warning);
+}
+
+void AFountain::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+	ULOG_S(Warning);
+}
+
 // Called every frame
 void AFountain::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-
+	//AddActorLocalRotation(FRotator(0.0f, RotateSpeed * DeltaTime, 0.0f));
 
 
 }
--- Source/UnrealTutorial/Fountain.h
+++ Source/UnrealTutorial/Fountain.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "UnrealTutorial.h"
+#include "GameFramework/RotatingMovementComponent.h"
 #include "GameFramework/Actor.h"
 #include "Fountain.generated.h"
 
@@ -18,6 +19,8 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+	virtual void PostInitializeComponents() override;
 
 public:	
 	// Called every frame
@@ -37,4 +40,11 @@ public:
 
 	UPROPERTY(EditAnywhere, Category = ID)
 	int32 ID;
+
+	UPROPERTY(VisibleAnywhere)
+	URotatingMovementComponent* Movement;
+
+private:
+	UPROPERTY(EditAnywhere, Category = Stat, Meta = (AllowPrivateAccess = true))
+	float RotateSpeed;
 };
